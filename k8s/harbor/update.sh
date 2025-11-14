6de2a56620207c8c1a620197861e0726240e1999
dano <thong.danghoang@elca.vn>
2025-11-15 03:18:14 +0700

    Add Harbor OIDC update script

---
+++ k8s/harbor/update.sh
@@ -0,0 +1,17 @@
+curl -u "admin:April03,2001" \
+  -X PUT \
+  -H "Content-Type: application/json" \
+  https://harbor.local/api/v2.0/configurations \
+  -d '{
+        "auth_mode": "oidc_auth",
+        "oidc_client_id": "harbor",
+        "oidc_endpoint": "https://keycloak.local/realms/orbstack",
+        "oidc_client_secret": "",
+        "oidc_logout": true,
+        "oidc_name": "keycloak",
+        "oidc_scope": "openid",
+        "oidc_verify_cert": false,
+        "primary_auth_mode": false,
+        "oidc_groups_claim": "groups",
+        "oidc_admin_groups": "/harbor-admins"
+      }'
