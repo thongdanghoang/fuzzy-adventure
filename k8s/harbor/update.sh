ecdaeb594e3ef3beff2faa03169d7cef75fd29bf
dano <thong.danghoang@elca.vn>
2025-11-16 01:10:42 +0700

    Disable TLS and SSL redirect in Harbor ingress

--- k8s/harbor/update.sh
+++ k8s/harbor/update.sh
@@ -5,12 +5,12 @@ curl -u "admin:Harbor12345" \
   -d '{
         "auth_mode": "oidc_auth",
         "oidc_client_id": "harbor",
-        "oidc_endpoint": "https://keycloak.local/realms/orbstack",
-        "oidc_client_secret": "6XIETELLe6BI9DzLi4rmmpFrV6KAkRSV",
+        "oidc_endpoint": "https://keycloak-thongdhse160015-dev.apps.rm3.7wse.p1.openshiftapps.com/realms/thongdhse160015",
+        "oidc_client_secret": "",
         "oidc_logout": true,
         "oidc_name": "keycloak",
         "oidc_scope": "openid",
-        "oidc_verify_cert": false,
+        "oidc_verify_cert": true,
         "primary_auth_mode": false,
         "oidc_groups_claim": "groups",
         "oidc_admin_groups": "/harbor-admins"
